3fae4dc0b051ec91ac1c706aeed869f315669ad1
Nicola Antonio Filannino <filanico@gmail.com>
2023-05-03 10:43:41 +0200

    Added shapes

--- engine.js
+++ engine.js
@@ -1,10 +1,11 @@
+
 let alert_shown = false;
 
 function GameSettings() {
     this.blockColorIdle = '#efefef';
     this.blockColorActive = '#c0c0c0';
     this.blockSizePx = 10;
-    this.fps = 4;
+    this.fps = 8;
     this.refreshEvery = 1 / this.fps * 1000;
 }
 
@@ -35,9 +36,12 @@ function Stage(width, height, left, top, gapX, gapY) {
     this.gapX = gapX;
     this.gapY = gapY;
     this.blocks = [];
-    this.shapes = [];
+    // this.shapes = [];
+    this.shape = null;
     this.settings = new GameSettings();
     this.block_sz = this.settings.blockSizePx;
+    this.shapeSize = [0, 0];
+    this.shapePosition = [0, 0];
     this.draw = () => {
         for (let c = 0; c < this.width; c++) {
             this.blocks[c] = [];
@@ -58,46 +62,118 @@ function Stage(width, height, left, top, gapX, gapY) {
     this.turnOff = (x, y) => {
         this.blocks[x][y].setColor(this.settings.blockColorIdle);
     }
-    this.add = (shape) => {
-        this.shapes.push(
-            shape
-        )
-        shape.paint();
+    this.add = (shape, position) => {
+        // this.shapes.push(
+        //     shape
+        // )
+        this.shape = shape;
+        this.shapeSize = [
+            shape[0].length,
+            shape.length,
+        ];
+        this.shapePosition = position;
+    }
+    this.paint = (shapeMatrix) => {
+        for (let c = 0; c < this.shapeSize[0]; c++) {
+            for (let r = 0; r < this.shapeSize[1]; r++) {
+                if (shapeMatrix[r][c] === '*') {
+                    this.turnOn(this.shapePosition[1] + c, this.shapePosition[0] + r);
+                } else if (shapeMatrix[r][c] === ' ') {
+                    this.turnOff(this.shapePosition[1] + c, this.shapePosition[0] + r);
+                }
+            }
+        }
+    }
+    this.moveDown = (shape) => {
+        if (this.shapePosition[0] + this.shapeSize[1] < this.height) {
+            for (let c = 0; c < this.shapeSize[0]; c++)
+                this.turnOff(this.shapePosition[1] + c, this.shapePosition[0])
+            this.shapePosition[0]++;
+        } else {
+            this.onSpawnRandomBlock();
+        }
+    }
+    //_UTILS
+    this.randomInRange = (min, max) => {
+        return Math.trunc(Math.random() * (max - min) + min)
+    }
+    //_EVENTS
+    this.onSpawnRandomBlock = () => {
+        // console.log("onSpawnRandomBlock");
+        let randomShapeIndex = this.randomInRange(0, Shape._shapes.length);
+        let randomShape = Shape._shapes[randomShapeIndex];
+        let randomColumnIndex = this.randomInRange(0, this.width - randomShape[0].length);
+        this.add(randomShape, [0, randomColumnIndex]);
     }
     this.tick = () => {
-        setInterval(()=>{
-            this.shapes.forEach( shape => {
-                shape.moveDown();
-                shape.paint();
-            })
-        },this.settings.refreshEvery);
+        setInterval(() => {
+            this.moveDown(this.shape);
+            this.paint(this.shape)
+        }, this.settings.refreshEvery);
     }
 }
 
-// function Shape(stage) {
-//     this.move = () => {
+function Shape() { }
 
-//     }
-// }
+Shape._shapes = [
+    [
+        '***',
+        '*',
+        '*',
+    ],
+    [
+        '* *',
+        '* *',
+        '***',
+    ],
+    // [
+    //     '***',
+    //     '***',
+    //     '***',
+    // ],
+    [
+        '*  ',
+        '***',
+        '  *',
+    ],
+    // [
+    //     '**  ',
+    //     '*** ',
+    //     '  **',
+    // ],
+    // [
+    //     '  *  ',
+    //     ' *** ',
+    //     '*****',
+    //     ' *** ',
+    //     '  *  ',
+    // ],
+];
+
+
+Shape.GetById = (id) => {
+    return Shape._shapes[id];
+}
+
 function ShapeSquare(stage, width, height, left, top) {
-    this.stage=stage;
-    this.width=width;
-    this.height=height;
-    this.left=left;
-    this.top=top;
+    this.stage = stage;
+    this.width = width;
+    this.height = height;
+    this.left = left;
+    this.top = top;
     this.canMoveDove
 
     this.paint = () => {
         for (let c = 0; c < this.width; c++) {
             for (let r = 0; r < this.height; r++) {
-                this.stage.turnOn(this.left+c, this.top+r);
+                this.stage.turnOn(this.left + c, this.top + r);
             }
         }
     }
     this.moveDown = () => {
-        if(this.top+this.height < this.stage.height){
-            for(let c=0;c<this.width;c++)
-                this.stage.turnOff(this.left+c, this.top)
+        if (this.top + this.height < this.stage.height) {
+            for (let c = 0; c < this.width; c++)
+                this.stage.turnOff(this.left + c, this.top)
             this.top++;
         }
     }
@@ -107,8 +183,11 @@ window.onload = () => {
     let stage = new Stage(30, 30, 5, 10, 1.1, 1.1);
     stage.draw();
     // stage.turnOn(1, 1);
-    let shape = new ShapeSquare(stage, 3, 3, 1, 1);
-    stage.add(shape);
+    let shape = Shape.GetById(0);
+    // let shape = new ShapeSquare(stage, 3, 3, 1, 1);
+    let position = [0, 5]; // [row,col]
+    stage.add(shape, position);
+    stage.paint(shape);
     stage.tick();
     // shape.paint();
     // shape.moveDown();
